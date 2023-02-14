2fb619d1012f48d68645738cf62fedf6c03acf39
freen <freennnn@gmail.com>
2023-02-13 18:28:41 -0800

    impl: reverse int super basic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+function reverse (n) {
+    let reversed = "";
+    n = Math.abs(n);
+    while (n  > 0) {
+        reversed += n  % 10;
+        n = Math.floor(n/10);
+    }
+    return parseInt(reversed);
 }
+
+module.exports = reverse;
+
+console.log(reverse(169));
